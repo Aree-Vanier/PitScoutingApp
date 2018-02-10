51c02f760df74ffffb99ecd9c69bdf71db8a09c4
Aree Vanier <gregory@ajayinkingston.com>
2018-02-10 17:12:17 -0500

    Worked on updating robot page

--- app/src/main/java/ca/lakeeffect/pitscoutingapp/RobotPage.java
+++ app/src/main/java/ca/lakeeffect/pitscoutingapp/RobotPage.java
@@ -5,11 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-/**
- * Created by Ajay on 9/25/2016.
- */
 public class RobotPage extends Fragment{
+
+    Spinner mechanism;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -22,6 +23,11 @@ public class RobotPage extends Fragment{
         View view = inflator.inflate(R.layout.robot_page, container, false);
 
         view.setTag("page2");
+
+        mechanism = (Spinner) view.findViewById(R.id.mechanismType);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(), R.array.mechanisms, android.R.layout.simple_spinner_item);
+        mechanism.setAdapter(adapter);
 
 //        ((TextView) view.findViewById(R.id.autoPeg)).setTextSize(15);
         return view;
